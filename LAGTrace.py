81d164de0aaa147f6dc16151e2ab5f1e6a9b6c43
Patrik Olsson <patrik.olsson@pfsw.com>
2018-03-23 13:50:02 +0100

    Updated LAGTrace.py to automatically calculate based on dst mac which port channel it is. Ingress port is needed on first switch, but after that ingressport on every switch is calculated based on LLDP and egress port. Script requires the IPs in the list be in the order switches are connected to function properly.

--- LAGTrace.py
+++ LAGTrace.py
@@ -17,7 +17,6 @@ EAPI_METHOD = 'http'
 usage = 'usage: %prog [options]'
 op = optparse.OptionParser(usage=usage)
 op.add_option( '-i', '--deviceips', dest='hosts', action='store', help='list of host IP addresses, where to seek check LAG packet path', type='string')
-op.add_option( '-a', '--portchannel', dest='interface', action='store', help='LAG trunk for packet to trace', type='string')
 op.add_option( '-s', '--srcmac', dest='srcmac', action='store', help='Src MAC for packet to trace', type='string')
 op.add_option( '-d', '--dstmac', dest='dstmac', action='store', help='Dst MAC for packet to trace', type='string')
 op.add_option( '-e', '--ethertype', dest='ethertype', action='store', help='Ether type for packet to trace', type='string')
@@ -30,12 +29,12 @@ opts, _ = op.parse_args()
 #
 
 hosts = opts.hosts
-interface = opts.interface
 srcmac = opts.srcmac
 dstmac = opts.dstmac
 ethertype = opts.ethertype
 ingressport = opts.ingressport
 vlanid = opts.vlanid
+firstrun = 1
 
 #
 # Creat a list of IPs from variable hosts
@@ -48,11 +47,24 @@ iplist = hosts.split()
 #
 
 for ip in iplist:
- switch = Server( '%s://%s:%s@%s/command-api' % ( EAPI_METHOD, EAPI_USERNAME, EAPI_PASSWORD, ip ) )
- response = switch.runCmds(1, ["enable", "show hostname", "show load-balance destination port-channel " + interface + " src-mac "+ srcmac + " dst-mac " + dstmac + " eth-type " + ethertype + " ingress-interface " + ingressport + " vlan " + vlanid])
- 
- hostname = response[1]["hostname"]
- egressinterface = response[2]["outputIntf"]
+	switch = Server( '%s://%s:%s@%s/command-api' % ( EAPI_METHOD, EAPI_USERNAME, EAPI_PASSWORD, ip ) )
 
- print "Hostname: %s" % ( hostname )
- print "Egress interface: %s" % ( egressinterface )
+	response = switch.runCmds(1, ["enable", "show mac address-table address " + dstmac])
+	maclist = response[1]["unicastTable"]["tableEntries"]
+	for mac in maclist:
+		if mac["macAddress"] == dstmac:
+			interface = mac["interface"]
+
+	response = switch.runCmds(1, ["enable", "show hostname", "show load-balance destination port-channel " + interface + " src-mac "+ srcmac + " dst-mac " + dstmac + " eth-type " + ethertype + " ingress-interface " + ingressport + " vlan " + vlanid])
+	hostname = response[1]["hostname"]
+	egressinterface = response[2]["outputIntf"]
+	
+	print "Hostname: %s" % ( hostname )
+	print "Port Channel: %s" % ( interface )
+	print "Egress Port channel member: %s" % ( egressinterface )
+
+	response = switch.runCmds(1, ["enable", "show lldp neighbors"])
+	neighborlist = response[1]["lldpNeighbors"]
+	for neighbor in neighborlist:
+		if neighbor["port"] == egressinterface:
+			ingressport = neighbor["neighborPort"]
