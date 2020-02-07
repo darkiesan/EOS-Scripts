59801e18828444fdb176b2941a7156d7f6da93bc
Patrik Olsson <po@arista.com>
2020-02-07 09:45:57 +0100

    First version of an EOS script that emotely trigger the config checkpoint function

---
+++ checkpoint.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import jsonrpclib,argparse
+
+#
+# Define command line options for argparse
+#
+
+usage = 'usage: %prog [options]'
+
+ap = argparse.ArgumentParser()
+ap.add_argument(
+    "-u",
+    "--username",
+    dest="username",
+    action="store",
+    required=True,
+    help="EOS username",
+)
+
+ap.add_argument(
+    "-i",
+    "--ipaddresscvpusername",
+    dest="ipaddress",
+    action="store",
+    required=True,
+    help="EOS device IP address",
+)
+
+ap.add_argument(
+    "-p",
+    "--password",
+    dest="password",
+    action="store",
+    required=True,
+    help="EOS password",
+)
+
+opts = ap.parse_args()
+
+ip = opts.ipaddress
+user = opts.username
+password = opts.password
+
+switch = Server( '%s://%s:%s@%s/command-api' % ( 'http', user, password, ip ) )
+result = switch.runCmds(1, ['enable' , 'configuration checkpoint save'])
+print result
